12e2730ea4f332082861deea3a8460993f28cc36
Kimlee Galarneau <kimlee.ny@hotmail.com>
2021-02-10 15:17:43 -0500

    icons-material-ui

--- src/Signet.jsx
+++ src/Signet.jsx
@@ -1,4 +1,6 @@
 import './Signet.scss';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+// import SortRoundedIcon from '@material-ui/icons/SortRounded';
 
 export default function Signet(props) {
     return (
@@ -7,6 +9,9 @@ export default function Signet(props) {
         <div className="info">
             <p className="titre">{props.titre}</p>
             <p className="dateModification">{props.dateModification}</p>
+            <button>
+                <MoreVertIcon />
+            </button>
         </div>
     </li>
     );
